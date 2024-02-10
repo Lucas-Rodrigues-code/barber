6fa049f2806a044e9c557adcd2a33a75c5b40478
Lucas Rodrigues de Souza <82272466+Lucas-Rodrigues-code@users.noreply.github.com>
2024-02-09 21:25:09 -0300

    :sparkles: footer

---
+++ app/components/Footer/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Box, Button, Flex, Icon, Text } from "@chakra-ui/react";
+import Image from "next/image";
+
+export function Footer() {
+  return (
+    <Flex
+      w={"100%"}
+      h={"412px"}
+      background={"#06121E"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Flex
+        w={"511px"}
+        h={"326px"}
+        direction={"column"}
+        align={"center"}
+        paddingTop={30}
+      >
+        <Flex justifyContent={"space-between"} w={"120px"}>
+          <Image
+            src="./assets/footer/instagram.svg"
+            alt="logo-instagram"
+            width={33}
+            height={33}
+          />
+          <Image
+            src="./assets/footer/facebook.svg"
+            alt="logo-instagram"
+            width={33}
+            height={33}
+          />
+          <Image
+            src="./assets/footer/twitter.svg"
+            alt="logo-instagram"
+            width={33}
+            height={33}
+          />
+        </Flex>
+        <Box paddingTop={30}>
+          <Image
+            src="./assets/footer/logo-barber.svg"
+            alt="logo-instagram"
+            width={166}
+            height={196}
+          />
+        </Box>
+        <Text
+          as="h3"
+          
+          textAlign={"center"}
+          fontFamily={"inter"}
+          fontWeight={700}
+         
+          marginTop={40}
+        >
+          Copyright© 2024 | Lucas-code - Todos os direitos reservados.
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
